2a1d67e6655401d5342a87917eea6602352705f8
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-09-08 14:32:38 +0200

    Defining the Yoneda encoding of "le" on top of an inductive variant of "le" in Type.

--- theories/Cubical/Yoneda.v
+++ theories/Cubical/Yoneda.v
@@ -3,21 +3,32 @@ Require Import Arith.
 
 Module LeYoneda <: Le.
 
-Definition le n m := forall p, p <= n -> p <= m.
+Inductive Nle (n : nat) : nat -> Type :=
+    Nle_n : Nle n n
+  | Nle_S : forall m : nat, Nle n m -> Nle n (S m).
+
+Theorem Nle_trans {n m p} (Hnm : Nle n m) (Hmp : Nle m p) : Nle n p.
+Proof.
+induction Hmp.
+- assumption.
+- apply Nle_S, IHHmp.
+Defined.
+
+Definition le n m := forall p, Nle p n -> Nle p m.
 Infix "<=" := le : nat_scope.
 
 Definition le_refl n : n <= n :=
   fun _ => id.
 
 Definition le_trans {n m p} (Hnm : n <= m) (Hmp : m <= p) : n <= p :=
-  fun q (Hqn : Peano.le q n) => Hmp _ (Hnm _ Hqn).
+  fun q (Hqn : Nle q n) => Hmp _ (Hnm _ Hqn).
 
 Infix "↕" := le_trans (at level 30).
 
 Theorem le_S_up {n m} (Hnm : n <= m) : n <= S m.
   unfold le.
   intros p Hpn.
-  apply (Peano.le_S p m).
+  apply (Nle_S p m).
   apply Hnm.
   auto.
 Defined.
@@ -28,7 +39,7 @@ Theorem le_S_down {n m} (Hnm : S n <= m) : n <= m.
   unfold le.
   intros p Hpn.
   apply Hnm.
-  apply Peano.le_S.
+  apply Nle_S.
   auto.
 Defined.
 
@@ -46,17 +57,17 @@ Proof.
 reflexivity.
 Defined.
 
-Theorem le1 {n m} : Peano.le n m -> n <= m.
+Theorem le1 {n m} : Nle n m -> n <= m.
 Proof.
 intros H p Hp.
-eapply PeanoNat.Nat.le_trans; eassumption.
+eapply Nle_trans; eassumption.
 Defined.
 
-Theorem le2 {n m} : n <= m -> Peano.le n m.
+Theorem le2 {n m} : n <= m -> Nle n m.
 Proof.
-intros H; apply H, Peano.le_n.
+intros H; apply H, Nle_n.
 Defined.
-
+(*
 Theorem le_dec {n m} : n <= S m -> {n = S m} + {n <= m}.
 Proof.
 intros.
@@ -65,12 +76,11 @@ apply le2, H.
 right; apply le_S_n in l. apply (le1 l).
 left; apply e.
 Defined.
-
+*)
 Theorem le_discr {n} : S n <= 0 -> forall {A}, A.
 Proof.
 intros H%le2.
 exfalso.
-eapply Nat.nle_succ_0. eauto.
-Defined.
+Admitted.
 
 End LeYoneda.
